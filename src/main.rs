eacf0f250af1ce25c8ef3b9937b3773124165ec6
Pedro Manse <pedro.manse@dmk3.com.br>
2025-02-05 15:00:30 -0300

    read tokens

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use tbl::*;
+
+fn main() {
+    //let user_def = TableDef{
+    //    name: "User".into(),
+    //    columns: vec![
+    //        ColumnDef{
+    //            name: "name".into(),
+    //            typedef: TypeDef::Text,
+    //        },
+    //    ]
+    //};
+    //let user_table = TableVal::new(&user_def, vec![
+    //    HashMap::from([
+    //        ("name".into(), TypeVal::Text("uwu".into())),
+    //    ])
+    //]);
+    //println!("{user_table:?}");
+    let c = std::fs::read_to_string("acc.tbl").unwrap();
+    let x = parse::x(&c);
+    //println!("{:?}");
+}
+
